68d556a9774325c53c0266259063a76318565dd6
Frontend JavaScript <frontendjavascript@iMac-06.local>
2024-03-07 20:41:43 +0200

    mywebsite

--- proiect/assets/js/main.js
+++ proiect/assets/js/main.js
@@ -9,17 +9,20 @@ const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
+      toggleSwitch.checked = true;
+      
     }
 }
 
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+      document.getElementById('logo').src='assets/img/logo.jpeg'
     }
     else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
+      localStorage.setItem('theme', 'light');
+      document.getElementById('logo').src='assets/img/logo1.jpeg'
     }    
 }
 
